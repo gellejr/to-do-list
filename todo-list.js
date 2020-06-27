cfde3ddcb2638b858d10ef4a05b439c5627fe8b9
gellejr <67238140+gellejr@users.noreply.github.com>
2020-06-27 16:39:13 +0300

    Add files via upload

---
+++ todo-list.js
@@ -0,0 +1,26 @@
+// check off specific  Todos by clicking
+$("ul").on("click", "li" , function(){
+  $(this).toggleClass("completed");
+});
+
+//click on X to delete Todo
+$("ul").on("click" , "span" , function(event){
+$(this).parent().fadeOut(500,function(){
+$(this).remove();
+});
+event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(events){
+if(event.which === 13){
+//grabing new todo text from input
+ var todoText = $(this).val();
+ $(this).val("");
+ //create a new li and add to ul
+ $("ul").append("<li><span><i class='fa fa-trash'></i> </span> " + todoText + "</li>")
+}
+});
+
+$(".fa-plus").click(function(){
+$("input[type='text'").fadeToggle();
+})
